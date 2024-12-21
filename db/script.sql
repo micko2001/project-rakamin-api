7c98b2053cc7264f0522f78caaba4661b7f04518
micko2001 <micko.wijayanto57@gmail.com>
2024-12-21 17:22:51 +0700

    feat: setup for vercel

--- db/script.sql
+++ db/script.sql
@@ -3,22 +3,22 @@ CREATE TABLE users (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     username character varying(20) NOT NULL UNIQUE,
     name character varying(255) NOT NULL,
-    password text,
-    avatar character varying(255),
-    point INT DEFAULT 0
+    password text NOT NULL,
+    avatar text NOT NULL,
+    point INT NOT NULL DEFAULT 0 
 );
 
 CREATE TABLE rooms (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     player1_id BIGINT NOT NULL REFERENCES users(id),
     player2_id BIGINT REFERENCES users(id),
-    game_status character varying(20) NOT NULL CHECK (game_status IN ('waiting', 'playing','finished', 'invalid')),
-    hand_position_p1 character varying(20) NOT NULL CHECK (hand_position_p1 IN ('paper', 'rock','scissor')),
-    hand_position_p2 character varying(20) NOT NULL CHECK (hand_position_p2 IN ('paper', 'rock','scissor')),
+    game_status character varying(20) ,
+    hand_position_p1 character varying(20) ,
+    hand_position_p2 character varying(20) ,
     draw BOOLEAN DEFAULT false,
     win INT,
     lose INT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    initialize_at TIMESTAMP DEFAULT NOW()
-
+    created_at TIMESTAMP DEFAULT NULL,
+    initialize_at TIMESTAMP DEFAULT NULL,
+    finish_at TIMESTAMP DEFAULT NULL
 )
